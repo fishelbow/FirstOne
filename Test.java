9c72ee151dbb542b603a54a0093ef0dffa475ab6
fishelbow <fishelbow@gmail.com>
2025-03-11 20:05:47 -0400

    just syncing

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+
+
+        // new edtion 
+
+        for (int i =1; i<5; i++){
+            System.out.println("Hellow, World!");
+        }
+
+
+    }
+}
